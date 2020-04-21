a226c3765af280df8db5568e465c0ad5106ebb7c
Miguel Salinas Gancedo <miguel@thingtrack.com>
2020-04-22 00:19:41 +0200

    last update

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,4 @@
-export * from './ping.controller';
 export * from './cmp-campanna.controller';
 export * from './gen-cliente.controller';
 export * from './gen-marca.controller';
+export * from './ping.controller';
